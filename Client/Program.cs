0604a7ff9c9dfe8a59902466ed38da2482dc9597
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-07 18:34:33 -0400

    Haciendo que las notificaciones funcionen en el index

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Miguel_API1_P2.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
