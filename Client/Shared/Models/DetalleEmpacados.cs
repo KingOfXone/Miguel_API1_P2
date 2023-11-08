14c9364256ea0b5774af2996776cebcd7e8dd6e9
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-08 01:29:44 -0400

    Prueba 2

--- Client/Shared/Models/DetalleEmpacados.cs
+++ Client/Shared/Models/DetalleEmpacados.cs
@@ -4,7 +4,7 @@ public class DetalleEmpacados
 {
     [Key]
     public int DetalleEmpacadosId { get; set; }
-    public int EmpacadosId { get; Set; }
+    public int EmpacadosId { get; set; }
     public int ProductoId { get; set; }
     [Required(ErrorMessage ="La Cantidad es requerida.")]
     [Range(1, double.MaxValue, ErrorMessage = "La Cantidad debe ser mayor que cero.")]
